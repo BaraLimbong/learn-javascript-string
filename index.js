631281a5914e980a3512eac6c5c7304edbbde6e6
Bara Limbong <baralimbong@gmail.com>
2019-01-31 09:26:14 +0700

    javascript string

---
+++ index.js
@@ -0,0 +1,25 @@
+const word1 = 'please welcome'
+const word2 = 'dinocode'
+console.log(word1)
+
+console.log(word2)
+
+const result = word1 + word2
+console.log(result)
+
+console.log('---------')
+
+const student = ['jo', 'mario', 'sakti', 'ari', 'bara', 'ajin']
+const best = student[4] + 'best student'
+console.log(best)
+const good = student[2] + student[0] + student[2] + 'its so good'
+console.log(good)
+const bad = student[3] + student[5] + 'its not so good'
+console.log(bad)
+
+console.log('--------')
+
+const tense = new String(
+  'semenjak mengenal impact byte saya jadi terbiasa untuk coding dan mengembangkan kemampuan saya terimakasih impact byte'
+)
+console.log(tense.toUpperCase())
